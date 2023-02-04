12e6a0600fb6abc54f95b2072f70fc2f1c79b4c3
ValdoVentman <valdo.ventman@tktk.ee>
2023-02-04 15:59:01 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is acomment to my code
+
+//rakendus küsib kasutajal sisestada tema nimi
+//rakendus tervitab kasutajat mimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+//string interpol´ation
+
+Console.WriteLine($"Hello, {userName} !"); //output
